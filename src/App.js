23d6b5a4782fdaf6775a9be0323400e653833deb
Mohamed Medhat <mohamedmmedhat21@gmail.com>
2021-07-27 09:28:52 +0200

    adding events on click & input section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
 const particlesOptions = {
   particles: {
@@ -17,19 +19,38 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles
-        params={particlesOptions}
-        className="particles"
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+    console.log(this.state.input);
+  }
+  onButtonSubmit=(event)=>{
+    this.setState({ imageUrl: event.target.value });
+    console.log('clicks');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          params={particlesOptions}
+          className="particles"
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+    return (
+      <div className='center ma'>
+
+      </div>
+    );
+  }
+  
+  export default FaceRecognition;
